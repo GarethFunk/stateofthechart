7b32acb89c733add20a599a038fb36a6dee499a0
EmmaRocheteau <ecr38@cam.ac.uk>
2017-09-30 19:22:57 +0100

    Revert "second commit! checks words against a shit dictionary"
    This reverts commit 7fef8d6398655ce18178f3268453106b58d5cb53.

--- improc/ocr.py
+++ improc/ocr.py
@@ -3,35 +3,5 @@ try:
 except ImportError:
     from PIL import Image
 import pytesseract
-from unidecode import unidecode
-import enchant
-import re
 
-#text = unidecode(pytesseract.image_to_string(Image.open('../tests/text_imgs/2.png')))
-
-text = unidecode("Sta rt my Fl ow Cha rt") #test string
-
-text = re.sub("(^|\W)\d+($|\W)", " ", text) #removes numbers from the string
-
-d = enchant.Dict("en_US")
-
-words = text.split()
-
-cleanedwords = []
-l = len(words)
-marker = []
-
-for i in range(l):
-    if i not in marker:
-        if d.check(words[i]) is True or i == l - 1:
-            cleanedwords.append(words[i])
-        else:
-            if d.check(words[i]+words[i+1]) is True:
-                cleanedwords.append(words[i]+words[i+1])
-                marker.append(i+1)
-            else:
-                cleanedwords.append(words[i])
-    else:
-        break
-
-print(cleanedwords)
+print(pytesseract.image_to_string(Image.open('../tests/text_imgs/test.png')))
