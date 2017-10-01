e57993633e8d07cbf8ddb84c6d93733cf2352ac2
EmmaRocheteau <ecr38@cam.ac.uk>
2017-10-01 12:01:06 +0100

    comments

--- improc/ocr.py
+++ improc/ocr.py
@@ -12,7 +12,7 @@ def read_words(words_file):
 
 #takes an image and returns handwritten text within the image, if unable to read, returns "UNABLE TO READ"
 def ocr(image):
-    text = unidecode(pytesseract.image_to_string(image))
+    text = unidecode(pytesseract.image_to_string(image)) #takes out accents and strange characters and replaces them with the nearest option
 
     text = text.lower()
     text = text.replace("5", "s").replace("7", "t").replace("sm", "sta")
@@ -31,12 +31,12 @@ def ocr(image):
 
     for i in range(l):
         if i not in marker:
-            if words[i] in eng_dict or i == l - 1:
+            if words[i] in eng_dict or i == l - 1: #if the word is in the dictionary or it is the last word in the string
                 cleanedwords.append(words[i])
             else:
-                if (words[i]+words[i+1]) in eng_dict:
+                if (words[i]+words[i+1]) in eng_dict: #if when concatenated with the subsequent word it forms a word
                     cleanedwords.append(words[i]+words[i+1])
-                    marker.append(i+1)
+                    marker.append(i+1) #update marker to skip the next iteration
                 else: cleanedwords.append(words[i])
 
     '''
@@ -64,7 +64,7 @@ def ocr(image):
         return cleanedwords
 
 if __name__ == '__main__':
-    image = Image.open('../tests/flowchart_images/take2.png')
+    image = Image.open('../tests/flowchart_images/two_boxes.png')
 
     text = ocr(image)
     print(text)
