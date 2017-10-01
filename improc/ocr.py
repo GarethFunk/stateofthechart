bf5bdbc46623de0015e64228ad69c1c8b5c6d98a
EmmaRocheteau <ecr38@cam.ac.uk>
2017-10-01 10:29:35 +0100

    §§

--- improc/ocr.py
+++ improc/ocr.py
@@ -7,9 +7,12 @@ from unidecode import unidecode
 import enchant
 import re
 
-#text = unidecode(pytesseract.image_to_string(Image.open('../tests/text_imgs/2.png')))
 
-text = unidecode("Sta rt my Fl ow Cha rt") #test string
+text = unidecode(pytesseract.image_to_string(Image.open('../tests/text_imgs/1.png')))
+
+text = text.lower()
+
+#text = unidecode("Sta rt my Fl ow Cha rt".lower()) #test string
 
 text = re.sub("(^|\W)\d+($|\W)", " ", text) #removes numbers from the string
 
@@ -21,6 +24,23 @@ cleanedwords = []
 l = len(words)
 marker = []
 
+def read_words(words_file):
+    return [word for line in open(words_file, 'r') for word in line.split()]
+
+eng_dict = read_words('../tests/words/google-10000-english-no-swears.txt')[:5000]
+
+for i in range(l):
+    if i not in marker:
+        if words[i] in eng_dict or i == l - 1:
+            cleanedwords.append(words[i])
+        else:
+            if (words[i]+words[i+1]) in eng_dict:
+                cleanedwords.append(words[i]+words[i+1])
+                marker.append(i+1)
+            else: cleanedwords.append(words[i])
+
+'''
+#code for US dictionary - very liberal with word inclusions
 for i in range(l):
     if i not in marker:
         if d.check(words[i]) is True or i == l - 1:
@@ -31,7 +51,6 @@ for i in range(l):
                 marker.append(i+1)
             else:
                 cleanedwords.append(words[i])
-    else:
-        break
+'''
 
 print(cleanedwords)
